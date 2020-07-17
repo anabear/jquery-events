66ba8f5979e93bb6c24c39a541bc11d70c2ca766
Anamaria <anamaria.butler@shopify.com>
2020-07-17 11:09:40 +0000

    jquery-more examples

--- script.js
+++ script.js
@@ -1,20 +1,106 @@
-$(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
-   });
-   $("#stream2_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream2").addClass('highlight_stream');
-   });
-   $("#stream3_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream3").addClass('highlight_stream');
-   });
-}); 
+//waits until page is loaded first
+$(document).ready(function(){
+
+    //applies colour red to paragraphs when clicked on 
+    $("p").click(function(){
+        $("p").css('color', 'red');
+    });
+
+    //will add lightblue to h2 elements
+    $("h2").hover(function(){
+        $("h2").css('background-color', 'lightblue'); 
+    });
+
+    /*
+    this will apply larger font size to the active h2 element 
+    by adding the h2_font_size class but 
+    not the other h2 elements by removing class h2_font_size from them
+    */
+    $("#hr_html").hover(function(){
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '2em');
+    });
+
+    $("#hr_mysql").hover(function(){
+        $("#hr_mysql").css('font-size', '2em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '1em');
+    });
+
+    $("#hr_python").hover(function(){
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '2em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '1em');
+    });
+
+    $("#hr_jquery").hover(function(){
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '2em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '1em');
+    });
+
+    $("#hr_django").hover(function(){
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '2em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '1em');
+    });
+
+    $("#hr_css").hover(function(){
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '2em');
+        $("#hr_html").css('font-size', '1em');
+    });
+
+    //applies colour black to body background when mouse enters over buttons
+    $(".bottom_button").mouseenter(function(){
+        $("body").css( "background-color", "black"); 
+    });
+
+    //applies colour grey to body background when mouse leaves buttons
+    $(".bottom_button").mouseleave(function(){
+        $("body").css( "background-color", "#eee"); 
+    });
+});
+
+//more examples
+$("#myButton").removeClass("blueBox").addClass("border");
+$("p").css("color","blue").slideUp(2000).slideDown(2000);
+$("a").attr("href","http://www.example.com");
+//The attr() function acts both as a getter and a setter.
+//As with the CSS function, we can query with one parameter, here we're looking for an href, 
+//and update the attribute for the second parameter, in this case, to example.com
+
+
+//more examples > inclusing this
+$(".stream-nav").on("click", function() {
+    // A selector to match all cards in all streams
+    var allStreamsCardsSelector = ".card";
+    // A selector to match just this stream's cards
+    // for this, we use the class with the name of the stream,
+    // which is the same as this nav link's id and then the "-card" suffix.
+    var thisStreamCardsSelector = "." + this.id + "-card";
+
+    // First remove the highlight from all of the cards
+    $(allStreamsCardsSelector).removeClass("card-highlight");
+    // Then apply the highlight to just this stream's cards
+    $(thisStreamCardsSelector).addClass("card-highlight");
+});
